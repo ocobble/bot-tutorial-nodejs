d88a4cedb6a1227ec067f57634f15b6bcf8325c2
Olivia Cobble <ocobble@hotmail.com>
2018-11-12 01:31:38 -0600

    Revert "Sharkfied this bot!"
    This reverts commit b775ded835883ab72897ceef2a8ad0191fb61751.

--- bot.js
+++ bot.js
@@ -6,26 +6,32 @@ var botID = process.env.BOT_ID;
 var counter = 0;
 
 var verses = [
-"Baby shark, doo doo doo doo doo doo Baby shark, doo doo doo doo doo doo Baby shark, doo doo doo doo doo doo Baby shark!",
+"18 naked cowboys in the showers at Ram Ranch! Big hard throbbing c*cks wanting to be sucked! 18 naked cowboys wanting to be f*cked! Cowboys in the showers at Ram Ranch! On their knees wanting to suck cowboy c*cks! Ram Ranch really rocks!",
 
-"Mommy shark, doo doo doo doo doo doo Mommy shark, doo doo doo doo doo doo Mommy shark, doo doo doo doo doo doo Mommy shark!",
+"Hot hard buff cowboys their c*cks throbbing hard! 18 more wild cowboys out in the yard! Big bulging c*cks ever so hard!",
 
-"Daddy shark, doo doo doo doo doo doo Daddy shark, doo doo doo doo doo doo Daddy shark, doo doo doo doo doo doo Daddy shark!",
+"Orgy in the showers at Ram Ranch! Big hard throbbing c*cks ramming cowboy butt! Like a breeding ram wanting to rut!",
 
-"Grandma shark, doo doo doo doo doo doo Grandma shark, doo doo doo doo doo doo Grandma shark, doo doo doo doo doo doo Grandma shark!",
+"Big hard throbbing c*cks getting sucked real deep! Cowboys even getting f*cked in their sleep! Ram Ranch, it rocks! Cowboys love big hard throbbing c*cks!",
 
-"Grandpa shark, doo doo doo doo doo doo Grandpa shark, doo doo doo doo doo doo Grandpa shark, doo doo doo doo doo doo Grandpa shark! "
+"18 naked cowboys in the showers at Ram Ranch! Big hard throbbing c*cks wanting to be sucked! 18 naked cowboys wanting to be f*cked! Cowboys in the showers at Ram Ranch! On their knees wanting to suck cowboy c*cks! Ram Ranch really rocks!",
+
+"Hot hard buff cowboys their c*cks throbbing hard! 18 more wild cowboys out in the yard! Big bulging c*cks ever so hard!",
+
+"Orgy in the showers at Ram Ranch! Big hard throbbing c*cks ramming cowboy butt! Like a breeding ram wanting to rut!",
+
+"18 naked cowboys in the showers at Ram Ranch! Big hard throbbing c*cks wanting to be sucked! 18 naked cowboys wanting to be f*cked! Cowboys in the showers at Ram Ranch! On their knees wanting to suck cowboy c*cks! Ram Ranch really rocks!",
+
+"Big hard throbbing c*cks getting sucked real deep! Cowboys even getting f*cked in their sleep! Ram Ranch, it rocks! Cowboys love big hard throbbing c*cks!",
+
+"Yeah 28 US Marines pulling up in black Ford Raptor Trucks! Helicopters landed! Ram Ranch is under seige under lockdown! US Marines are gonna f*ck ram ranch cowboy butts! Looking for Prince Harry! Gunna fuck prince harrys butt! Yeah wild buff! Cool US Marines gonna f*ck cowboy butts!"
 ];
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      babyRegex = /baby/;
-      mommyRegex = /mommy/;
-      daddyRegex = /daddy/;
-      grandmaRegex = /grandma/;
-      grandpaRegegx = /grandpa/;
+      botRegex = /ram ranch/;
 
-  if(request.text && babyRegex.test(request.text)) {
+  if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
@@ -39,8 +45,17 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq, myRand;
 
-  botResponse = verses[0];
+  //botResponse = '18 naked cowboys in the showers at Ram Ranch! Big hard throbbing c*cks wanting to be sucked! 18 naked cowboys wanting to be f*cked! Cowboys in the showers at Ram Ranch! On their knees wanting to suck cowboy c*cks! Ram Ranch really rocks!';
 
+	//botResponse = cool();
+	//counter = counter + 1;
+	//botResponse = getverse();
+
+	// Here's a random comment!
+
+	myRand = Math.floor(Math.random() * verses.length);     // 
+returns a random integer from 0 to 8
+	botResponse = verses[myRand];
   options = {
     hostname: 'api.groupme.com',
     path: '/v3/bots/post',
