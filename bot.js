b97b4f11bbcdbec4e48e2ba702344f2698247435
Olivia Cobble <ocobble@hotmail.com>
2019-01-05 21:51:47 -0600

    Ram ranch-ified this bot!

--- bot.js
+++ bot.js
@@ -6,22 +6,35 @@ var botID = process.env.BOT_ID;
 var counter = 0;
 
 var verses = [
-"It's just a gas station",
-"Yes it is"
+"18 naked cowboys in the showers at Ram Ranch! Big hard throbbing c*cks wanting to be sucked! 18 naked cowboys wanting to be f*cked! Cowboys in the showers at Ram Ranch! On their knees wanting to suck cowboy c*cks! Ram Ranch really rocks!",
+
+"Hot hard buff cowboys their c*cks throbbing hard! 18 more wild cowboys out in the yard! Big bulging c*cks ever so hard!",
+
+"Orgy in the showers at Ram Ranch! Big hard throbbing c*cks ramming cowboy butt! Like a breeding ram wanting to rut!",
+
+"Big hard throbbing c*cks getting sucked real deep! Cowboys even getting f*cked in their sleep! Ram Ranch, it rocks! Cowboys love big hard throbbing c*cks!",
+
+"18 naked cowboys in the showers at Ram Ranch! Big hard throbbing c*cks wanting to be sucked! 18 naked cowboys wanting to be f*cked! Cowboys in the showers at Ram Ranch! On their knees wanting to suck cowboy c*cks! Ram Ranch really rocks!",
+
+"Hot hard buff cowboys their c*cks throbbing hard! 18 more wild cowboys out in the yard! Big bulging c*cks ever so hard!",
+
+"Orgy in the showers at Ram Ranch! Big hard throbbing c*cks ramming cowboy butt! Like a breeding ram wanting to rut!",
+
+"18 naked cowboys in the showers at Ram Ranch! Big hard throbbing c*cks wanting to be sucked! 18 naked cowboys wanting to be f*cked! Cowboys in the showers at Ram Ranch! On their knees wanting to suck cowboy c*cks! Ram Ranch really rocks!",
+
+"Big hard throbbing c*cks getting sucked real deep! Cowboys even getting f*cked in their sleep! Ram Ranch, it rocks! Cowboys love big hard throbbing c*cks!",
+
+"Yeah 28 US Marines pulling up in black Ford Raptor Trucks! Helicopters landed! Ram Ranch is under seige under lockdown! US Marines are gonna f*ck ram ranch cowboy butts! Looking for Prince Harry! Gunna fuck prince harrys butt! Yeah wild buff! Cool US Marines gonna f*ck cowboy butts!"
+
 ];
 
 function respond() {
   var   request = JSON.parse(this.req.chunks[0]),
-        QTRegex = /QT/i;
-  var	antiQTRegex = /No its not/;
+        myRegex = /ram ranch/;
 
-  if(request.text && QTRegex.test(request.text)) {
+  if(request.text && myRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(1);
-    this.res.end();
-  }  else if (request.text && antiQTRegex.test(request.txt)) {
-    this.res.writeHead(200);
-    postMessage(2);
+    postMessage();
     this.res.end();
   } else {
     console.log("don't care");
@@ -30,7 +43,7 @@ function respond() {
   }
 }
 
-function postMessage(val) {
+function postMessage() {
   var botResponse, options, body, botReq, myRand;
 
   //botResponse = '18 naked cowboys in the showers at Ram Ranch! Big hard throbbing c*cks wanting to be sucked! 18 naked cowboys wanting to be f*cked! Cowboys in the showers at Ram Ranch! On their knees wanting to suck cowboy c*cks! Ram Ranch really rocks!';
@@ -41,8 +54,8 @@ function postMessage(val) {
 
 	// Here's a random comment!
 
-	//myRand = Math.floor(Math.random() * verses.length); // returns a random integer from 0 to 8
-	//botResponse = verses[myRand];
+	myRand = Math.floor(Math.random() * verses.length); // returns a random integer from 0 to 8
+	botResponse = verses[myRand];
 	botResponse = verses[0];
   options = {
     hostname: 'api.groupme.com',
